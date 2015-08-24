9b3d34e3ddd251544ec89897fda788a63dd67516
vivek maskara <maskaravivek@hotmail.com>
2015-08-24 23:56:29 +0530

    Wrap Grid for Windows Phone 8 and 8.1

---
+++ Wrap Grid WP8/Wrap Grid WP8/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Wrap_Grid_WP8.Resources;
+
+namespace Wrap_Grid_WP8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            AddItems();
+        }
+        private void AddItems()
+        {
+            List<string> items = new List<string>
+            {
+                "Delhi NCR",
+                "Bangalore",
+                "Bombay",
+                "Chennai",
+                "Mangalore",
+                "Hyderabad",
+                "Kolkata",
+                "Patna",
+                "Goa",
+                "Pune",
+                "Lahore"
+            };
+            LocationItemsControl.ItemsSource = items;
+        }
+    }
+}
